a2b2f5a76efbbef7c57dd4d0f6f7df711d43e795
Taner Özer <tanerozer16@gmail.com>
2022-03-26 13:16:28 +0300

    index and about page

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ const express = require("express");
 
 const app = express();
 
+// teplate engine 
+app.set("view engine", "ejs");
+
+// middleware
+app.use(express.static("pulbic"))
+
 const port = 3000;
 
+
 app.get("/", (req, res) => {
-   res.status(200).send("index sayfasi")
+    res.status(200).render("index",{
+        page_name:"index",
+    })
 })
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about',
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`App Started ${port}`)
