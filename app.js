28bdac8157becab04c68cb69dae29373990a4d11
Taner Özer <tanerozer16@gmail.com>
2022-03-26 15:52:57 +0300

    mongo models

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute")
+const courseRoute = require("./routes/courseRoute")
 
 const app = express();
-const pageRoute = require("./routes/pageRoute")
 
-// teplate engine 
+mongoose.connect('mongodb://localhost/smartedu-db',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+}).then(()=>{
+    console.log("DB connect succesfully")
+});
+
+// template engine 
 app.set("view engine", "ejs");
 
 // middleware
 app.use(express.static("pulbic"))
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); 
+// for parsing application/x-www-form-urlencoded
+
 
 // Routes
 app.use("/",pageRoute)
+app.use("/courses",courseRoute)
+
 
 const port = 3000;
 app.listen(port, () => {
