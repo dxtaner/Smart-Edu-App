f09ce66f8aaf821138f3ee676e66736a468f230a
Taner Özer <tanerozer16@gmail.com>
2022-03-26 15:05:30 +0300

    courseControllers

---
+++ controllers/courseController.js
@@ -0,0 +1,19 @@
+const Course = require('../models/Course');
+
+exports.createCourse = async (req, res) => {
+
+    try {
+        const course = await Course.create(req.body);
+        res.status(201).json({
+            status: "success",
+            course,
+        })
+    }
+    catch {
+        res.status(400).json({
+            status: "fail",
+            error,
+        })
+    }
+
+}
