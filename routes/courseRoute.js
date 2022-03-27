b1a9d03020d731b87bf2905d1dd1910acefa78e3
Taner Özer <tanerozer16@gmail.com>
2022-03-27 16:05:53 +0300

    course routes and controllers

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/").post(courseController.createCourse);
 router.route("/").get(courseController.getAllCourses);
+router.route("/:slug").get(courseController.getCourse);
 
 module.exports = router;
