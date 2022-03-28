c0a2973c955eca3e1b701f9f231ab345f993e24b
Taner Özer <tanerozer16@gmail.com>
2022-03-28 19:50:32 +0300

    app js edited

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require("./routes/pageRoute")
 const courseRoute = require("./routes/courseRoute")
 const categoryRoute = require("./routes/categoryRoute");
 const userRoute = require("./routes/userRoute");
 const app = express();
 
-mongoose.connect('mongodb://localhost/smartedu-db',{
+
+mongoose.connect('mongodb://localhost/smartedu-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useFindAndModify: false,
     // useCreateIndex: true,
-}).then(()=>{
+}).then(() => {
     console.log("DB connect succesfully")
 });
 
 // template engine 
 app.set("view engine", "ejs");
 
+// global variable 
+global.userIN = null;
+
 // middleware
 app.use(express.static("pulbic"))
 app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 // for parsing application/x-www-form-urlencoded
-
+app.use(
+    session({
+        secret: 'my_keyboard_cat', // Buradaki texti değiştireceğiz.
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' })
+    })
+);
 
 // Routes
-app.use("/",pageRoute)
-app.use("/courses",courseRoute)
-app.use("/categories",categoryRoute)
-app.use("/users",userRoute)
+app.use('*', (req, res, next) => {
+    userIN = req.session.userID;
+    next();
+});
+app.use("/", pageRoute)
+app.use("/courses", courseRoute)
+app.use("/categories", categoryRoute)
+app.use("/users", userRoute)
+
+
 
 
 const port = 3000;
