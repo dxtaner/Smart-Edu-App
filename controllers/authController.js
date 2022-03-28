a06b1e85c21002de051214a8c41a035ea8f22670
Taner Özer <tanerozer16@gmail.com>
2022-03-28 16:56:31 +0300

    loginuser add

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,3 +15,25 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    await User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            res.status(200).send('YOU ARE LOGGED IN');
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
