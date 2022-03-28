eebecf972be4971f0a6762509618ee9034dd1590
Taner Özer <tanerozer16@gmail.com>
2022-03-28 16:53:30 +0300

    page login add

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -16,5 +16,12 @@ exports.getRegisterPage = (req, res) => {
     });
 };
 
+exports.getLoginPage = (req, res) => {
+    res.status(200).render('login', {
+        page_name: 'login',
+    });
+};
+
+
 
 
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.route("/").get(pageController.getİndexPage);
 router.route("/about").get(pageController.getAboutPage);
 router.route('/register').get(pageController.getRegisterPage);
+router.route('/login').get(pageController.getLoginPage);
 
 module.exports = router;
