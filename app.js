6a053b9a1d8c9388862f3c9863ffaa20d56c7ea8
Taner Özer <tanerozer16@gmail.com>
2022-03-28 16:19:37 +0300

    app js edited

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute")
 const courseRoute = require("./routes/courseRoute")
-
+const categoryRoute = require("./routes/categoryRoute");
 const app = express();
 
 mongoose.connect('mongodb://localhost/smartedu-db',{
@@ -27,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/",pageRoute)
 app.use("/courses",courseRoute)
+app.use("/categories",categoryRoute)
 
 
 const port = 3000;
