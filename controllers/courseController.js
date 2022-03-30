5c1ff7287e12769b82cabf94ff35a1594f8c9612
Taner Özer <tanerozer16@gmail.com>
2022-03-30 21:59:09 +0300

    release eklendi

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
 
@@ -51,10 +52,11 @@ exports.getAllCourses = async (req, res) => {
 exports.getCourse = async (req, res) => {
     try {
         const course = await Course.findOne({ slug: req.params.slug }).populate('user')
-
+        const user = await User.findById({slug:req.session.userID})
         res.status(200).render('course', {
             course,
             page_name: 'courses',
+            user
         });
     } catch (error) {
         res.status(400).json({
@@ -64,19 +66,33 @@ exports.getCourse = async (req, res) => {
     }
 };
 
-
 exports.enrollCourse = async (req, res) => {
     try {
-  
-      const user = await User.findById(req.session.userID);
-      await user.courses.push({_id:req.body.course_id});
-      await user.save();
-  
-      res.status(200).redirect('/users/dashboard');
+
+        const user = await User.findById(req.session.userID);
+        await user.courses.push({ _id: req.body.course_id });
+        await user.save();
+
+        res.status(200).redirect('/users/dashboard');
     } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        error,
-      });
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
     }
-  };
+};
+
+exports.releaseCourse = async (req, res) => {
+    try {
+        const user = await User.findById(req.session.userID);
+        await user.courses.pull({ _id: req.body.course_id });
+        await user.save();
+
+        res.status(200).redirect('/users/dashboard');
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
