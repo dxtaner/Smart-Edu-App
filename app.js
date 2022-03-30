2f3e89fab37bee2f63bbf7081223122f285266be
Taner Özer <tanerozer16@gmail.com>
2022-03-30 22:16:40 +0300

    edited

--- app.js
+++ app.js
@@ -48,9 +48,6 @@ app.use("/courses", courseRoute)
 app.use("/categories", categoryRoute)
 app.use("/users", userRoute)
 
-
-
-
 const port = 3000;
 app.listen(port, () => {
     console.log(`App Started ${port}`)
