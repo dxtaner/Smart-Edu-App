b356e325998ca1c2b4badfb5434c79b45ddfe9e3
Taner Özer <tanerozer16@gmail.com>
2022-03-31 22:44:18 +0300

    flash mesajlar eklendi

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -30,41 +30,50 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendEmail = async (req, res) => {
-    const outputMessage = `
-    <h1>Mail Details </h1>
-    <ul>
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-    </ul>
-    <h1>Message</h1>
-    <p>${req.body.message}</p>`
 
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-          user: "tanerozerly@gmail.com", // gmail hesabı
-          pass: "jejxddpqprkjiojo", // gmail şifresi veya uygulama şifresi
-        },
-      });
+    try {
+        const outputMessage = `
+        <h1>Mail Details </h1>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+        </ul>
+        <h1>Message</h1>
+        <p>${req.body.message}</p>
+        `
 
-      // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: '"Smart EDU Contact Form" <tanerozerly@gmail.com>', // sender address
-        to: "tanerultraslan@yandex.com", // list of receivers
-        subject: "Smart EDU Contact Form New Message ✔", // Subject line
-        html: outputMessage, // html body
-    });
-    
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  
-    res.status(200).redirect('contact');
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user: "tanerozerly@gmail.com", // gmail hesabı
+                pass: "jejxddpqprkjiojo", // gmail şifresi veya uygulama şifresi
+            },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Smart EDU Contact Form" <tanerozerly@gmail.com>', // sender address
+            to: "tanerultraslan@yandex.com", // list of receivers
+            subject: "Smart EDU Contact Form New Message ✔", // Subject line
+            html: outputMessage, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+        req.flash("success", "We Received your message succesfully");
+
+        res.status(200).redirect('contact');
+
+    } catch (err) {
+        req.flash("error", `Something happened!`);
+    }
 };
 
 
