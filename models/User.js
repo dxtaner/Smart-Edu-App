09b183f57157611b1eac15caa9d62464540db2fb
Taner Özer <tanerozer16@gmail.com>
2022-04-01 18:12:41 +0300

    user model edited

--- models/User.js
+++ models/User.js
@@ -35,5 +35,19 @@ UserSchema.pre('save', function (next) {
   })
 })
 
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(10, function(err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+          user.password = hash;
+          next();
+      });
+  });
+});
+
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
