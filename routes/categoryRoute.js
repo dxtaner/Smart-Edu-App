964a7daf51c2e181bc69a662f43b40ed69f10cbe
Taner Özer <tanerozer16@gmail.com>
2022-04-02 17:24:21 +0300

    category deleted

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,5 +4,7 @@ const categoryController = require("../controllers/categoryController");
 const router = express.Router();
 
 router.route("/").post(categoryController.createCategory);
+router.route('/:id').delete(categoryController.deleteCategory);
+
 
 module.exports = router;
