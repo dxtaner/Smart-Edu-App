520dd2634a90b4be2fe8c9f640391117f677f967
Taner Özer <tanerozer16@gmail.com>
2022-04-02 19:19:27 +0300

    user routes duzenlendi

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,22 +5,25 @@ const { body } = require('express-validator');
 const User = require("../models/User");
 
 const router = express.Router();
+
+
 router.route('/signup').post(
     [
         body('name').not().isEmpty().withMessage('please enter your name'),
 
         body('email').isEmail().withMessage('please enter valid email')
-        .custom((userEmail)=> {
-            return User.findOne({email:userEmail}).then(user => {
-                if (user) {
-                    return Promise.reject('Email is already exists!')
-                }
-            })
-        }),
-        
+            .custom((userEmail) => {
+                return User.findOne({ email: userEmail }).then(user => {
+                    if (user) {
+                        return Promise.reject('Email is already exists!')
+                    }
+                })
+            }),
+
         body('password').not().isEmpty().withMessage('please enter a password'),
     ]
-    ,authController.createUser);
+    , authController.createUser);
+
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
